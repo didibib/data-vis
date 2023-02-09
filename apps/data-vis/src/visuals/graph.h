4c9e867382aec495f845228ebdcd9517f06541b7
tariq <tariquake@hotmail.com>
2023-02-09 22:42:27 +0100

    Switched to adjacency matrix

--- apps/data-vis/src/visuals/graph.h
+++ apps/data-vis/src/visuals/graph.h
@@ -3,23 +3,19 @@ namespace DataVis {
 	class Node {
 	public:
 		Node() = default;
-		uint NeighborCount();
-
-		ofVec3f position;
-		int stride;
-		std::vector<int> data;
 	};
 
 	class Graph {
 		std::vector<string> headers_;
-		std::unordered_map<int, Node> nodes_;
+		std::unordered_map<int, std::vector<int>> adjacency_;
+		int stride_ = 0;
 
 	public:
 		Graph() = default;
-		void setup(std::string filename);
-		void update();
-		void draw();
-		void exit();
+		void Setup(std::string filename);
+		void Update();
+		void Draw();
+		void Exit();
 
 		int sphereSize = 3;
 	};
