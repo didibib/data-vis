c69a97a152c490318d4d941d44b51f9e63d3418c
Tariq <tariquake@hotmail.com>
2023-02-15 11:15:28 +0100

    Can parse dot files, but had to include the whole boost librar

--- apps/data-vis/src/visuals/graph.cpp
+++ apps/data-vis/src/visuals/graph.cpp
@@ -1,18 +1,27 @@
 #include "precomp.h"
 
+// Graph parsing
+#include "read_graphviz_new.cpp"
+
 namespace DataVis {
 	void Graph::Setup(std::string _filename) {
 		std::string filepath = ofToDataPath(_filename, false);
 		std::ifstream file( filepath );
-		if ( file.is_open() ) {
-			int x;
-		}
 		
 		if ( !std::filesystem::exists( filepath ) ) {
 			std::cout << "W/Graph::Setup: File doesn't exists: " << filepath << std::endl;
 			return;
 		}
-		DotGraph graph = DotParser::ParseFromFile( filepath );
+
+		graph_u graph( 0 );
+		boost::dynamic_properties dp( boost::ignore_other_properties );
+		dp.property( "node_id", get( &Vertex::name, graph ) );
+		dp.property( "label", get( &Vertex::label, graph ) );
+		dp.property( "shape", get( &Vertex::shape, graph ) );
+		dp.property( "label", get( &Edge::label, graph ) );
+		dp.property( "weight", get( &Edge::weight, graph ) );
+
+		boost::read_graphviz( file, graph, dp );
 	}
 
 	void Graph::Update() {
