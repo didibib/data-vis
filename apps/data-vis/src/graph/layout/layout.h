31f01521aeb5ab2f3d99f7a7a6fa8e1008c57e10
Tariq <tariquake@hotmail.com>
2023-03-14 12:45:22 +0100

    Vertex positioning wip

--- apps/data-vis/src/graph/layout/layout.h
+++ apps/data-vis/src/graph/layout/layout.h
@@ -94,36 +94,69 @@ class Sugiyama : public Layout
 {
 public:
 	bool Gui( IStructure& ) override;
-	static void Apply( Graph& );
+	static void Apply( Graph&, int oscm_iterations, float delta_x );
 private:
 	using Layer = std::vector<int>;
-	using GetNeighbors = std::function<std::vector<Neighbor> (Vertex&)>;
+	using GetNeighbors = std::function<std::vector<Neighbor>( Vertex& )>;
 
 	static const int VisitedIdx;
 	static const std::string DummyId;
 	static const int RemoveIdx;
 
+	int m_oscm_iterations = 100;
+
+	// Sugiyama Framework
 	static Dataset BreakCycles( Dataset& );
 	static void LayerAssignment( Dataset&, std::vector<Layer>& vertices_per_layer, Layer& layer_per_vertex );
-	static Dataset AddDummyVertices( Dataset&, std::vector<Layer>& vertices_per_layer, Layer& layer_per_vertex );
-	static void CrossingMinimization( Dataset&, std::vector<Layer>& _vertices_per_layer, Layer& _layer_per_vertex);
-	static void VertexPositioning( Graph& );
+	static void AddDummyVertices( Dataset&, std::vector<Layer>& vertices_per_layer, Layer& layer_per_vertex );
+	static int CrossingMinimization( Dataset&, std::vector<Layer>& vertices_per_layer, int iterations );
+	static void VertexPositioning( Dataset& _dataset, std::vector<Layer>& vertices_per_layer, Layer& layer_per_vertex, float delta_x );
 
+	//--------------------------------------------------------------
 	// Layer Assignment
+	//--------------------------------------------------------------
 	static bool IsSink( Vertex& );
 	static bool IsSource( Vertex& );
 	static bool Has( std::function<bool( Vertex& )>, std::vector<Vertex>, Vertex& out );
 	static void RemoveOutgoingNeighbors( Dataset&, Vertex& );
 	static void RemoveIncomingNeighbors( Dataset&, Vertex& );
 	static void RemoveNeighbors( Dataset&, Edge& );
-	static void AddNeighbors( Dataset&, Edge&);
+	static void AddNeighbors( Dataset&, Edge& );
 
+	//--------------------------------------------------------------
 	// OSCM
+	//--------------------------------------------------------------
 	static bool BarycenterHeuristic( Dataset&, std::vector<float>& all_coords, Layer& layer_fixed, Layer& layer, Layer& new_layer,
 		GetNeighbors get_neighbors,
 		GetNeighbors get_reverse_neighbors );
 
 	static int Crossings( Dataset&, Layer& layer_1, Layer& layer_2 );
 
+	//--------------------------------------------------------------
+	// Node Positioning
+	//--------------------------------------------------------------
+	static void FlagType1Conflicts(
+		Dataset&, std::vector<Layer>& vertices_per_layer,
+		Layer& layer_per_vertex,
+		std::vector<std::pair<int, int>>& flags );
+
+	static void VerticalAlignment(
+		Dataset&,
+		std::vector<Layer>& vertices_per_layer,
+		Layer& layer_per_vertex,
+		Layer& pos_per_vertex,
+		std::vector<int>& root,
+		std::vector<int>& align,
+		std::vector<std::pair<int, int>>& flags );
+
+	static void HorizontalCompaction(
+		Dataset&,
+		std::vector<Layer>& vertices_per_layer,
+		Layer& layer_per_vertex,
+		Layer& pos_per_vertex,
+		std::vector<int>& root,
+		std::vector<int>& align, 
+		std::vector<float>& x_per_vertex,
+		float delta);
 };
 } // namespace DataVis
